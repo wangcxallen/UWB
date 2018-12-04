d4c9b4deaf9250cebed1420b83087aca82e85414
Allen <39018805+wangcxallen@users.noreply.github.com>
2018-12-04 03:33:35 -0500

    Add files via upload

--- mqtt/mqtt-host.py
+++ mqtt/mqtt-host.py
@@ -13,13 +13,13 @@ PORT = 1883
 
 epoch = 0
 squence_num = 0
-while(epoch<5):
+while(epoch<1000):
     for i in np.arange(3):
         print("RPI %i speaks..." % i)
         print("MSG %i" % squence_num)
         print("-"*30)
         publish.single("UWB", "%i%i"% (i, squence_num), hostname=HOST)
         squence_num += 1
-        time.sleep(2)
+        time.sleep(10)
     epoch +=1
 
